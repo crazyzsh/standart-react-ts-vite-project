1e0cdaa6fcbdcf4f72b342b99e64ecad8e2131c4
郑上华 <zhengshanghua@picc.com.cn>
2024-03-25 14:06:03 +0800

    📃 docs(readme):暂停commitlint使用

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -16,7 +16,7 @@ module.exports = {
 				// 重构
 				'refactor',
 				// 文档
-				'📃docs',
+				'docs',
 				// 构建过程或者辅助工具的变动,如增加依赖库等
 				'chore',
 				// 不影响代码运行的变动
@@ -37,4 +37,71 @@ module.exports = {
 		'subject-case': [0, 'never'],
 		'header-max-length': [0, 'always', 74],
 	},
-};
+//   prompt: {
+// 		questions: {
+// 			type: {
+// 				description: "Select the type of change that you're committing",
+// 				enum: {
+// 					feat: {
+// 						description: 'A new feature',
+// 						title: 'Features',
+// 						emoji: '✨',
+// 					},
+// 					fix: {
+// 						description: 'A bug fix',
+// 						title: 'Bug Fixes',
+// 						emoji: '🐛',
+// 					},
+// 					docs: {
+// 						description: 'Documentation only changes',
+// 						title: 'Documentation',
+// 						emoji: '📚',
+// 					},
+// 					style: {
+// 						description:
+// 							'Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)',
+// 						title: 'Styles',
+// 						emoji: '💎',
+// 					},
+// 					refactor: {
+// 						description:
+// 							'A code change that neither fixes a bug nor adds a feature',
+// 						title: 'Code Refactoring',
+// 						emoji: '📦',
+// 					},
+// 					perf: {
+// 						description: 'A code change that improves performance',
+// 						title: 'Performance Improvements',
+// 						emoji: '🚀',
+// 					},
+// 					test: {
+// 						description: 'Adding missing tests or correcting existing tests',
+// 						title: 'Tests',
+// 						emoji: '🚨',
+// 					},
+// 					build: {
+// 						description:
+// 							'Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)',
+// 						title: 'Builds',
+// 						emoji: '🛠',
+// 					},
+// 					ci: {
+// 						description:
+// 							'Changes to our CI configuration files and scripts (example scopes: Travis, Circle, BrowserStack, SauceLabs)',
+// 						title: 'Continuous Integrations',
+// 						emoji: '⚙️',
+// 					},
+// 					chore: {
+// 						description: "Other changes that don't modify src or test files",
+// 						title: 'Chores',
+// 						emoji: '♻️',
+// 					},
+// 					revert: {
+// 						description: 'Reverts a previous commit',
+// 						title: 'Reverts',
+// 						emoji: '🗑',
+// 					},
+// 				},
+// 			},}
+// }
+}
